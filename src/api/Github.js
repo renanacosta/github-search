9838469eb5077bfd8e368cb2da91356b9e72c372
Renan Acosta <renangomesacosta@gmail.com>
2022-01-12 09:42:27 -0300

    Created Avatar
    Creation of component avatar and cards to show user, repository and favorites.

--- src/api/Github.js
+++ src/api/Github.js
@@ -4,7 +4,7 @@ const api = axios.create({
     baseURL: 'https://api.github.com/users/',
     headers: {
         Accept: 'application/vnd.github.v3.raw',
-        Authorization: `Token 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`,
+        Authorization: `Token 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`,
     },
 });
 
